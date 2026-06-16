f7aafca721864b59d9f07fdab30adeff66410450
Arjan <97673925+AreArjane@users.noreply.github.com>
2026-06-16 19:55:18 +0200

    Update critical

--- language-culture-astro-template/astro.config.mjs
+++ language-culture-astro-template/astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://arearjane.github.io',
-  base: '/cultural-website-interactive-HASKTECH',
+  site: 'https://example.org'
 });
